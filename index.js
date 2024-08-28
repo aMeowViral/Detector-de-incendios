fa0968fc27b3227b9b92590425ce86a593a88cc8
aMeowViral <148305883+aMeowViral@users.noreply.github.com>
2024-08-28 12:31:04 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// Obtén una referencia al botón
+var boton = document.getElementById('miBoton');
+
+// Agrega un escuchador de eventos al botón
+boton.addEventListener('click', function() {
+  // Redirige a la página deseada al hacer clic en el botón
+  window.location.href = 'detector.html';
+});
